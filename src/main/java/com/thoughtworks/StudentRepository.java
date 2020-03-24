b3130151f3da1061a11b65a0595a0d30a8c4e479
lpdft <772346730@qq.com>
2020-03-25 00:45:29 +0800

    [ruyue] feat: implement save() method with Student parameter

--- src/main/java/com/thoughtworks/StudentRepository.java
+++ src/main/java/com/thoughtworks/StudentRepository.java
@@ -1,5 +1,8 @@
 package com.thoughtworks;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +14,20 @@ public class StudentRepository {
 
   public void save(Student student) {
     // TODO:
+    Connection conn = DbUtil.getConnection();
+    String sql = "INSERT INTO student (id,name,gender,admission_year,birthday,class_id) values(?,?,?,?,?,?)";
+    try {
+      PreparedStatement ptmt = conn.prepareStatement(sql);
+      ptmt.setString(1, student.getId());
+      ptmt.setString(2, student.getName());
+      ptmt.setString(3, student.getGender());
+      ptmt.setInt(4, student.getAdmissionYear());
+      ptmt.setDate(5, new Date(student.getBirthday().getTime()));
+      ptmt.setString(6, student.getClassId());
+      ptmt.execute();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   public List<Student> query() {
