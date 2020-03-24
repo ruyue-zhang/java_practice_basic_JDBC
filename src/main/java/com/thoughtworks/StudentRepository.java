e1d1fda4b69a11c6c86f681c3b262f70d58b463b
lpdft <772346730@qq.com>
2020-03-25 00:48:00 +0800

    [ruyue] feat: implement update() method

--- src/main/java/com/thoughtworks/StudentRepository.java
+++ src/main/java/com/thoughtworks/StudentRepository.java
@@ -86,6 +86,23 @@ public class StudentRepository {
 
   public void update(String id, Student student) {
     // TODO:
+    Connection conn = DbUtil.getConnection();
+    String sql = "UPDATE student SET id = ?, name = ?, gender = ?, admission_year = ?,  birthday = ?, class_id = ? WHERE id = ?";
+    try {
+      PreparedStatement ptmt = conn.prepareStatement(sql);
+
+      ptmt.setString(1, student.getId());
+      ptmt.setString(2, student.getName());
+      ptmt.setString(3, student.getGender());
+      ptmt.setInt(4, student.getAdmissionYear());
+      ptmt.setDate(5, new Date(student.getBirthday().getTime()));
+      ptmt.setString(6, student.getClassId());
+      ptmt.setString(7,id);
+
+      ptmt.execute();
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
   }
 
   public void delete(String id) {
